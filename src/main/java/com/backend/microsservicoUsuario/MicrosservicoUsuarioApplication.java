c337f96fe2f67ec0299658f136fe61f418cf1392
Willian dos Anjos Boaventura <williandosanjos@gmail.com>
2026-03-18 11:17:04 -0300

    Estrutura inicial do microserviço de usuario

---
+++ src/main/java/com/backend/microsservicoUsuario/MicrosservicoUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.backend.microsservicoUsuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosservicoUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosservicoUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/microsservicoUsuario/MicrosservicoUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.microsservicoUsuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosservicoUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
